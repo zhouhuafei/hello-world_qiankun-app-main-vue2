0cd8807d190dd47714d67d40dc100f3bda40c642
zhouhuafei <1123486116@qq.com>
2022-02-06 15:48:45 +0800

    feat: 🎸 暂时移除子应用的全部路由拦截

--- src/router/index.js
+++ src/router/index.js
@@ -18,21 +18,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
   },
-  // 下述两个路由的配置，目的是为了把子应用的404交给子应用自身处理。
-  // 用一个空模板占位符进行占位，防止渲染子应用时，把主应用的404渲染出来。
-  {
-    path: '/child1/*',
-    name: 'Child1Placeholder',
-    meta: { title: 'Child1Placeholder', hidden: true },
-    component: () => import(/* webpackChunkName: "Child1Placeholder" */ '@/views/MainPlaceholder.vue')
-  },
-  {
-    path: '/child2/*',
-    name: 'Child2Placeholder',
-    meta: { title: 'Child2Placeholder', hidden: true },
-    component: () => import(/* webpackChunkName: "Child2Placeholder" */ '@/views/MainPlaceholder.vue')
-  },
-  // 主应用的404。
   {
     path: '*',
     name: '404',
