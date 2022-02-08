27516afcc41140cf4ff30fb8b1b8859bb6a74dec
zhouhuafei <1123486116@qq.com>
2022-02-08 13:25:48 +0800

    fix: 🐛 不拿掉会报错

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ start({ sandbox: { experimentalStyleIsolation: true } })
 
 // 微前端 - 主子应用通信 - 加if是因为qiankun的v3版本会移除这个api
 if (initGlobalState) {
-  const state = { a: 1, b: 2, mainStore: store }
+  const state = { a: 1, b: 2 }
   const actions = initGlobalState(state)
   if (actions.onGlobalStateChange) {
     actions.onGlobalStateChange((state, prev) => { // 监听state的变更
